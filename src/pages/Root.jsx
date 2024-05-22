4c0b56f7f7926aaf1e4c23ea44d543c248f6a4ad
datle-dev <dle.dev@outlook.com>
2024-05-22 18:21:14 -0400

    Add state for count of items in cart and pass to children
    Create state variable for count and event handlers for adding and
    subtracting. Pass the state to the Navbar component where it will
    eventually be displayed with the cart icon. Pass the state and the
    event handlers as context to the Outlet that shows the remaining
    pages and be able to increment/decrement the parent's state variable
    from multiple pages.

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,12 +1,23 @@
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 
 const Root = () => {
+  const [count, setCount] = useState(0);
+
+  const handleAddCount = () => {
+    setCount(count + 1);
+  };
+
+  const handleSubtractCount = () => {
+    setCount(count - 1);
+  };
+
   return (
     <>
-      <Navbar />
+      <Navbar count={count}/>
       <main>
-        <Outlet />
+        <Outlet context={[count, handleAddCount, handleSubtractCount]}/>
       </main>
     </>
   );
