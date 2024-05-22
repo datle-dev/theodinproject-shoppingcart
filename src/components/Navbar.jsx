23f9d078b00956b2950ce6afd1a244560a406094
datle-dev <dle.dev@outlook.com>
2024-05-22 17:59:55 -0400

    Add semantic HTML tags to navigation and main content

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,11 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <>
+    <nav>
       <Link to="/">Home</Link>
       <Link to="/catalog">Catalog</Link>
       <Link to="/cart">Cart</Link>
-    </>
+    </nav>
   );
 };
 
--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
-import Navbar from './Navbar';
+import Navbar from '../components/Navbar';
 
 const Root = () => {
   return (
     <>
       <Navbar />
-      <Outlet />
+      <main>
+        <Outlet />
+      </main>
     </>
   );
 };
