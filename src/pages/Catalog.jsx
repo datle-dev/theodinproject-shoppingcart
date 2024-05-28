35e0a8d200511e341a6a8466fbe11d77383e3d19
datle-dev <dle.dev@outlook.com>
2024-05-28 11:50:40 -0400

    Add cart state with local storage and pass to catalog as provider

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,12 +1,18 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, createContext } from 'react';
 import { useOutletContext } from 'react-router-dom';
 import CatalogCard from '../components/CatalogCard';
+
+export const CartContext = createContext({});
 
 const Catalog = () => {
   const [count, handleAddCount, handleSubtractCount] = useOutletContext();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [products, setProducts] = useState(null);
+  const [cart, setCart] = useState(() => {
+    const storedCart = localStorage.getItem('cart');
+    return storedCart !== null ? JSON.parse(storedCart) : {};
+  });
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -35,13 +41,15 @@ const Catalog = () => {
       <h1>Catalog</h1>
       <button type="button" onClick={handleAddCount}>Add to Cart</button>
       <button type="button" onClick={handleSubtractCount}>Subtract from Cart</button>
-      {products.map((product) => {
-        return (
-          <article key={product.id}>
-            <CatalogCard product={product} />
-          </article>
-        )
-      })}
+      <CartContext.Provider value={[cart, setCart]}>
+        {products.map((product) => {
+          return (
+            <article key={product.id}>
+              <CatalogCard product={product} />
+            </article>
+          )
+        })}
+      </CartContext.Provider>
     </>
   );
 };
